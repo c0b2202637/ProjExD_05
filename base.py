61dfd97c167328b6d415b3ca50587e81951c1970
G <c0b2202637@edu.teu.ac.jp>
2023-07-11 14:08:37 +0900

    初期状態

---
+++ base.py
@@ -0,0 +1,35 @@
+import math
+import random
+import sys
+import time
+from typing import Any
+
+import pygame as pg
+from pygame.sprite import AbstractGroup
+
+
+#基本機能
+
+
+#障害物
+
+
+
+#スコア走行距離
+
+
+
+#空飛ぶ
+
+
+#無敵
+
+
+
+#障害物破壊
+
+
+#地形生成
+
+
+
