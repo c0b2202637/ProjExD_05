549a7ebd571277b90e3594c4a0fbb0392121bbb9
yuki <c0a2212499@edu.teu.ac.jp>
2023-07-18 14:20:10 +0900

    修正

--- base.py
+++ base.py
@@ -194,7 +194,7 @@ def main():
         bird.update(key_lst, screen)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 if __name__ == "__main__":
     pg.init()
